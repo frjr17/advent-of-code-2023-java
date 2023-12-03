06fc82512afa2a5fc3e390dfd225cf53c58629cd
Hernán Valencia <frjr17@criptext.com>
2023-12-02 22:58:16 -0500

    fix: changing name in main class

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         Collection<String> puzzle = TextFileReader.readTextFile("./puzzle.txt");
-        System.out.println("Advent of Code - Day 1\n");
+        System.out.println("Advent of Code - Day 2\n");
 
         System.out.print("Part 1 Result: ");
         System.out.println(One.getResult(puzzle));
