4b5be005ce9aef4695ef0edb8c619d7aab8ae0de
Hernán Valencia <frjr17@criptext.com>
2023-12-06 11:46:14 -0500

    fix: changing day string

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         List<String> puzzle = TextFileReader.readTextFile("./puzzle.txt");
-        System.out.println("Advent of Code - Day 3\n");
+        System.out.println("Advent of Code - Day 6\n");
 
         System.out.print("Part 1 Result: ");
         System.out.println(One.getResult(puzzle));
